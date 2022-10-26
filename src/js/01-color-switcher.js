de8695da86361c6b86d04c8db254ca3b56cfeba7
morozzA <masik.moroz@gmail.com>
2022-10-26 22:27:25 +0300

    1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,6 @@
+const startBtn = document.querySelector('data-start');
+console.log(startBtn);
 
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  }
