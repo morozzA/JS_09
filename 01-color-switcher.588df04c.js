155a4041efbdbddbda5d10e99566deb8c69b5a6b
morozzA <109750058+morozzA@users.noreply.github.com>
2022-10-26 19:27:51 +0000

    Deploying to gh-pages from @ morozzA/JS_09@de8695da86361c6b86d04c8db254ca3b56cfeba7 🚀

---
+++ 01-color-switcher.588df04c.js
@@ -0,0 +1,2 @@
+const o=document.querySelector("data-start");console.log(o);
+//# sourceMappingURL=01-color-switcher.588df04c.js.map
---
+++ 01-color-switcher.5b37d79a.js
@@ -0,0 +1,2 @@
+!function(){var o=document.querySelector("data-start");console.log(o)}();
+//# sourceMappingURL=01-color-switcher.5b37d79a.js.map
