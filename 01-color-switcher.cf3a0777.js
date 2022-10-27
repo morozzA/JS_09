8816a85d4cc106026b5369a1f8f19ab31b0c36fe
morozzA <109750058+morozzA@users.noreply.github.com>
2022-10-27 00:24:38 +0000

    Deploying to gh-pages from @ morozzA/JS_09@a9f7af1358d521b17fd1ff0df9198f8223a65c06 🚀

--- 01-color-switcher.cf3a0777.js
+++
@@ -1,2 +0,0 @@
-!function(){var t=document.querySelector("body"),e=document.querySelector("button[data-start]"),n=document.querySelector("button[data-stop]"),d=null;e.addEventListener("click",(function(o){d=setInterval((function(){t.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16))}),1e3),e.disabled=!0,n.disabled=!1})),n.addEventListener("click",(function(t){clearInterval(d),e.disabled=!1,n.disabled=!0})),n.disabled=!0}();
-//# sourceMappingURL=01-color-switcher.cf3a0777.js.map
