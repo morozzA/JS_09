07fa56bfc633358fb914603fa4b1ca52f9c96d31
morozzA <masik.moroz@gmail.com>
2022-10-27 03:12:26 +0300

    finish 1 exercise

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,5 +1,28 @@
-const startBtn = document.querySelector('data-start');
-console.log(startBtn);
+const body = document.querySelector('body');
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
+
+let timerId = null;
+
+startBtn.addEventListener('click', changeColorBody);
+stopBtn.addEventListener('click', stopChangeColorBody);
+
+stopBtn.disabled = true;
+
+function changeColorBody(e) {
+  timerId = setInterval(() => {
+      body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+  startBtn.disabled = true;
+  stopBtn.disabled = false;
+}
+
+function stopChangeColorBody(e) {
+  clearInterval(timerId);
+  startBtn.disabled = false;
+  stopBtn.disabled = true;
+
+}
 
 function getRandomHexColor() {
     return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
